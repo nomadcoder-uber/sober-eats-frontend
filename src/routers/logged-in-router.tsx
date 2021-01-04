a536e3cb889e9f6577af0ecd287af64f44e8d056
somin <qkrthals0524@naver.com>
2021-01-05 00:20:54 +0900

    15.18 Header part Two

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -2,8 +2,8 @@ import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
-import { meQuery } from "../__generated__/meQuery";
 
 
 const ClientRoutes = [
@@ -12,19 +12,9 @@ const ClientRoutes = [
   </Route>,
 ];
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
 
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  const { data, loading, error } = useMe();
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -37,7 +27,7 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === "Client" && ClientRoutes}
-        <Redirect from="/potato" to="/" />
+        <Redirect to="/" />
     </Switch>
   </Router>
   );
